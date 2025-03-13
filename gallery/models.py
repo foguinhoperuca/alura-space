5ee5f28a6e51428b7a10449b46e086a529bc9b1b
Jefferson Campos <jecampos@sorocaba.sp.gov.br>
2025-03-13 12:55:44 -0300

    feat: Django: Persistência de Dados e Admin.
    Class 01 finished.

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Photograph(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    legend = models.CharField(max_length=512, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    photo = models.CharField(max_length=128, null=False, blank=False)
+
+    def __str__(self):
+        return f"Photograph: [name={self.name}]"
--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from gallery.models import Photograph
+
 
 def index(request):
-    return render(request, 'gallery/index.html')
+    data = {
+        1: {
+            "name": "Carina's nebulosa",
+            "legend": "webtelescope.org / NASA / James Webb"
+        },
+        2: {
+            "name": "Galaxy NGC 1079",
+            "legend": "nasa.org / NASA / Hubble"
+        }
+    }
+
+    data_models = Photograph.objects.all()
+
+    # return render(request, 'gallery/index.html', {"cards": data})
+    return render(request, 'gallery/index.html', {"cards": data_models})
 
 
 def image(request):
